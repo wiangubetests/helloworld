2ebcc32500b71d4d31b88dff94987824e4aa7ab4
wiangube <wiangube@gmail.com>
2019-05-31 05:36:21 -0400

    Commit to trigger Jenkins build

--- src/main/java/com/wiangube/helloworld/controller/WebController.java
+++ src/main/java/com/wiangube/helloworld/controller/WebController.java
@@ -30,12 +30,4 @@ public class WebController {
 			return response;
 		}
 	}
-
-	@RequestMapping("/sample")
-	public SampleResponse Sample(@RequestParam(value = "name", defaultValue = "Robot") String name) {
-		SampleResponse response = new SampleResponse();
-		response.setId(1);
-		response.setMessage("Your name is" + name);
-		return response;
-	}
 }
